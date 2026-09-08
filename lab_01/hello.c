0d93bf7a5f8f4ebda222873551dc29880b68aca3
vedanshi-dev <vedanshidaima.19@gmail.com>
2026-09-08 11:52:38 +0530

    Add hello.c to print 'Hello, World!'

---
+++ lab_01/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Print Hello, World! to the console
+    printf("Hello, World!\n");
+    
+    // Indicate that the program ended successfully
+    return 0;
+}
