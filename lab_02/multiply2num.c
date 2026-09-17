1d60e76451268e356283015878c0d1e0d231c223
vedanshi-dev <vedanshidaima.19@gmail.com>
2026-09-17 10:12:23 +0530

    Add multiply2num.c to multiply two numbers

---
+++ lab_02/multiply2num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int a,b,c;
+  printf("enter no.1:");
+  scanf("%d",&a);
+  printf("enter no.2:");
+  scanf("%d",&b);
+  c=a*b;
+  printf("%d",c);
+  return 0;
+  
+}
